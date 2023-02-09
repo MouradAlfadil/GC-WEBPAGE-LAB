c6d192c1bf845fdf4992e4d54580e0adcb532575
unknown <mouradalfadil@gmail.com>
2023-02-09 17:04:53 -0500

    you will consume

---
+++ CoffeeShop Lab/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace CoffeeShop_Lab.Models
+{
+    public class User
+    {
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+}
